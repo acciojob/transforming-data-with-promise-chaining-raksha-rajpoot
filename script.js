a1400fbb82b4a677b3e7fdba8cd281b9a78dc24f
raksha-rajpoot <rakraj654@gmail.com>
2025-04-19 19:01:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 //your JS code here. If required.
+document.getElementById("btn").onclick = function () {
+  const input = document.getElementById("ip").value;
+  const outputDiv = document.getElementById("output");
+  outputDiv.innerText = "";
+
+  const number = Number(input);
+
+  if (isNaN(number)) {
+    outputDiv.innerText = "Please enter a valid number.";
+    return;
+  }
+
+  new Promise((resolve) => {
+    setTimeout(() => {
+      outputDiv.innerText = `Result: ${number}`;
+      resolve(number);
+    }, 2000);
+  })
+    .then((res) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const result = res * 2;
+          outputDiv.innerText = `Result: ${result}`;
+          resolve(result);
+        }, 1000); 
+      });
+    })
+    .then((res) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const result = res - 3;
+          outputDiv.innerText = `Result: ${result}`;
+          resolve(result);
+        }, 1000); 
+      });
+    })
+    .then((res) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const result = res / 2;
+          outputDiv.innerText = `Result: ${result}`;
+          resolve(result);
+        }, 1000); 
+      });
+    })
+    .then((res) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const result = res + 10;
+          outputDiv.innerText = `Final Result: ${result}`;
+          resolve(result);
+        }, 1000); 
+      });
+    });
+};
